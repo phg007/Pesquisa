12c13759cc5098e749f666ff1a369157f66fb3e5
Paulo Henrique Gonçalves <tantonioms@gmail.com>
2024-12-11 14:22:27 -0300

    Configurando a Porta do Banco

--- src/app/api/submit-survey/route.ts
+++ src/app/api/submit-survey/route.ts
@@ -4,7 +4,7 @@ import mysql from 'mysql2/promise';
 // Database connection configuration
 const dbConfig = {
   host: process.env.DB_HOST,
-  
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
