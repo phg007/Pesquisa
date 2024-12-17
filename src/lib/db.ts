793a323a27c1adb8f636c2b731b7b3366529fe81
Adm_desenvMM <paulo.henriqueicm2011@gmail.com>
2024-12-17 10:02:26 -0300

    alteracao title

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise';
 
 export const dbConfig = {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 54375,
+  // port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
