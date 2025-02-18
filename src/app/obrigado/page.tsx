062fa759763bc6d86909e2415e32d9294da213fa
Paulo Henrique Gonçalves <paulo.henriqueicm2011@gmail.com>
2025-02-18 11:19:09 -0300

    feat(Qrocev):changing the image thanks

--- src/app/obrigado/page.tsx
+++ src/app/obrigado/page.tsx
@@ -4,28 +4,19 @@ import Image from 'next/image'
 export default function ObrigadoPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <Card className="w-full max-w-md">
-        <CardHeader>
+    
           <div className="flex justify-center mb-6">
             <Image
-              src="/logo_cor.png"
+              src="/obrigado.png"
               alt="Mart Minas Logo"
-              width={200}
-              height={80}
+              width={400}
+              height={280}
               priority
             />
           </div>
-          <CardTitle className="text-2xl font-bold text-center">Obrigado por participar!</CardTitle>
-        </CardHeader>
-        <CardContent className="text-center">
-          <p className="mb-4">
-            Sua opinião é muito importante para nós. Agradecemos por dedicar seu tempo para responder nossa pesquisa.
-          </p>
-          <p className="mb-6">
-            Suas respostas nos ajudarão a melhorar nossos serviços e atender melhor às suas necessidades.
-          </p>
-        </CardContent>
-      </Card>
+    
+      
+     
     </div>
   )
 }
